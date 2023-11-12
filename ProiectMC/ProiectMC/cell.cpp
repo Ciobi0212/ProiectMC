4584c38e04afcc87b53b58cb0a7e7e2b3bd767dc
Ciobi0212 <cristian.ciobanu@student.unitbv.ro>
2023-11-12 11:28:04 -0800

    Implemented methods for Cell class.

---
+++ ProiectMC/ProiectMC/cell.cpp
@@ -0,0 +1,49 @@
+module cell;
+using twixt::Cell;
+
+import peg;
+using twixt::Peg;
+
+import link;
+using twixt::Link;
+
+import <optional>;
+import <string>;
+
+Cell::Cell() = default;
+
+Cell::~Cell() = default;
+
+std::optional<std::string> Cell::getColor() const {
+	return m_color;
+}
+
+std::optional<Peg*> Cell::getPeg() const {
+	return m_peg;
+}
+
+std::optional<Link*> Cell::getLink() const {
+	return m_link;
+}
+
+void Cell::setColor(const std::string& color) {
+	m_color.emplace(color);
+}
+
+void Cell::setPeg(const Peg& peg) {
+	m_peg.emplace(peg);
+}
+
+void Cell::setLink(const Link& link) {
+	m_link.emplace(link);
+}
+
+bool Cell::hasColor() const {
+	return m_color.has_value();
+}
+
+bool Cell::hasPeg() const {
+	return m_peg.has_value();
+}
+
+
